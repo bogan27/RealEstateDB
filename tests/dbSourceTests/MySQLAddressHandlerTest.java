3558ee2d7d26b22e515b330210cbe608ca994838
Brandon <brandon.bogan93@gmail.com>
2016-04-16 17:02:56 -0400

    Completed implementation and testing of AddressHandler

--- tests/dbSourceTests/MySQLAddressHandlerTest.java
+++ tests/dbSourceTests/MySQLAddressHandlerTest.java
@@ -6,6 +6,7 @@ package dbSourceTests;
 import static org.junit.Assert.*;
 
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.Map;
 
 import org.junit.Test;
@@ -53,7 +54,6 @@ public class MySQLAddressHandlerTest {
       assertTrue(valuesToSearchOn.get("address").length() > 0);
       assertTrue(valuesToSearchOn.get("zip").length() > 0);
     } catch (SQLException e) {
-      // TODO Auto-generated catch block
       e.printStackTrace();
     }
 
@@ -64,7 +64,28 @@ public class MySQLAddressHandlerTest {
    */
   @Test
   public void testProcessAddress() {
-    fail("Not yet implemented"); // TODO
+    MySQLAddressHandler testHandler;
+    try {
+      testHandler = new MySQLAddressHandler();
+
+      Map<String, String> valuesToProcess = testHandler.getNextAddressToUpdate();
+      valuesToProcess.put("api_result", Boolean.toString(true));
+      testHandler.processAddress(valuesToProcess);
+      Map<String, String> valuesAfterProcess = testHandler.getNextAddressToUpdate();
+
+      String addressBefore = valuesToProcess.get("address");
+      System.out.println("Address before: " + addressBefore);
+      String addressAfter = valuesAfterProcess.get("address");
+      System.out.println("Address after: " + addressAfter);
+      assertFalse(addressBefore.equals(addressAfter));
+
+      String zipBefore = valuesToProcess.get("zip");
+      String zipAfter = valuesAfterProcess.get("zip");
+      assertFalse(zipBefore.equals(zipAfter));
+
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
   }
 
 }
