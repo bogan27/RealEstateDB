994d49c4952ed6b936ced1d1432d8ac977f0d332
Ron Farizon <farizon.r@husky.neu.edu>
2016-07-18 22:19:55 -0400

    Validator fixes

--- src/main/java/dbConnectors/Validator.java
+++ src/main/java/dbConnectors/Validator.java
@@ -3,7 +3,5 @@ package main.java.dbConnectors;
 import main.java.classes_for_db.Property;
 
 public interface Validator {
-	
-	public boolean validateGeo(Property property, String geo);
-	public boolean validateComp(Property property);
+	public boolean validate(Property property);
 }
